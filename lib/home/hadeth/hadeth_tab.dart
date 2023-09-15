084ae3f7b56c82465258f3932665ec4efabbd11b
Lailaashraff <lailaashraf323@gmail.com>
2023-09-15 03:29:17 +0300

    finished hadeth tab and hadeth details screen.

--- lib/home/hadeth/hadeth_tab.dart
+++ lib/home/hadeth/hadeth_tab.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_app/home/hadeth/item_hadeth_name.dart';
 
-class HadethTab extends StatelessWidget {
+class HadethTab extends StatefulWidget {
+  @override
+  State<HadethTab> createState() => _HadethTabState();
+}
+
+class _HadethTabState extends State<HadethTab> {
+  List<Hadeth> hadethLines = [];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
+    if (hadethLines.isEmpty) {
+      loadHadethFile();
+    }
+    return Column(
+      children: [
+        Center(child: Image.asset('assets/images/hadethlogo.png')),
+        Divider(
+          color: Theme.of(context).primaryColor,
+          thickness: 3,
+        ),
+        Text(
+          'Hadeth Name',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        Divider(
+          color: Theme.of(context).primaryColor,
+          thickness: 3,
+        ),
+        Expanded(
+          child: hadethLines.isEmpty
+              ? Center(
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                )
+              : ListView.separated(
+                  separatorBuilder: (context, index) {
+                    return Divider(
+                      color: Theme.of(context).primaryColor,
+                      thickness: 3,
+                    );
+                  },
+                  itemBuilder: (context, index) {
+                    return ItemHadethName(hadeth: hadethLines[index]);
+                  },
+                  itemCount: hadethLines.length),
+        )
+      ],
     );
   }
+
+  void loadHadethFile() async {
+    String ahadeethContent =
+        await rootBundle.loadString('assets/files/ahadeth.txt');
+    List<String> ahadethList = ahadeethContent.split('#\r\n');
+    for (int i = 0; i < ahadethList.length; i++) {
+      // print(ahadethList[i]);
+      List<String> ahadethLines = ahadethList[i].split('\n');
+      String title = ahadethLines[0];
+      ahadethLines.removeAt(0);
+      Hadeth hadeth = Hadeth(title: title, content: ahadethLines);
+      hadethLines.add(hadeth);
+      setState(() {});
+    }
+  }
+}
+
+class Hadeth {
+  String title;
+  List<String> content;
+
+  Hadeth({required this.title, required this.content});
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/home/hadeth/hadeth_details_screen.dart';
 import 'package:islami_app/home/home_screen.dart';
 import 'package:islami_app/home/quran/sura_details_screen.dart';
 
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
         SuraDetailsScreen.routeName: (context) => SuraDetailsScreen(),
+        HadethDetailsScreen.routeName: (context) => HadethDetailsScreen()
       },
       theme: MyTheme.lightMode,
     );
